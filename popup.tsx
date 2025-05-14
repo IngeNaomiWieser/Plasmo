edd6df68ea3bdfb4d2214a67007216a42217ca34
Inge <inge@greenspacehealth.com>
2025-05-13 17:31:47 -0700

    update popup text

--- popup.tsx
+++ popup.tsx
@@ -9,16 +9,12 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
+       Hello World from Plasmo !
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      {/*<input onChange={(e) => setData(e.target.value)} value={data} />*/}
+      {/*<a href="https://docs.plasmo.com" target="_blank">*/}
+      {/*  View Docs*/}
+      {/*</a>*/}
     </div>
   )
 }
